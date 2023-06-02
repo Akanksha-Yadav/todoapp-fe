4b61a678be5190d5f573a93718ba690e6a3712e3
Your Name <you@example.com>
2023-06-02 16:53:04 +0530

    deletedone

--- front_end/todolist/src/toDoList.js
+++ front_end/todolist/src/toDoList.js
@@ -1,31 +1,17 @@
 import { useState } from "react";
 
 function ToDoList(props) {
-    const [newtoDoList,setnewtoDoList] = useState([]);
-    function ToDelete(key){
-               {props.toDoList.map(
-                   todo =>{
-                   if(todo[0]!== key)
-                   {
-                        newtoDoList.push([todo[0],todo[1]]);
-                        setnewtoDoList(newtoDoList)
-                   }
-                }
-               )}
-            props.setToDoList([])
-            console.log(newtoDoList);
-            {newtoDoList.map(
-                todo =>{ props.toDoList.push([todo[0],todo[1]])
-                //props.setToDoList(props.ToDoList)
-               }
-            )}
-            props.setToDoList(props.ToDoList)
-            console.log(props.toDoList);
-            setnewtoDoList([])   
-            console.log(newtoDoList);
-    }
-    
 
+    function deleteItem(key, toDoList, setToDoList) {
+        var newtoDoList = []
+        for (var i = 0; i<toDoList.length;i++) {
+            if (toDoList[i][0] != key) {
+                newtoDoList.push(toDoList[i])
+            }
+        }
+
+        setToDoList(newtoDoList)
+    }
 
     return (
         <div>
@@ -33,7 +19,7 @@ function ToDoList(props) {
                 {props.toDoList.map(
                     todo => <li key={todo[0]}>{todo[1]}
                    
-                    <button classname="delete-button" onClick={() => {ToDelete(todo[0])}}>Delete</button>
+                    <button classname="delete-button" onClick={() => {deleteItem(todo[0], props.toDoList, props.setToDoList)}}>Delete</button>
                     </li>
                 )}
             </ul>): (<div>No tasks</div>)}
