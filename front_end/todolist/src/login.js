8ca8b1d134b41e9437a6aaba0780da503cdaf276
Your Name <you@example.com>
2023-06-03 14:24:22 +0530

    login and todolist on same page

---
+++ front_end/todolist/src/login.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./login.css";
+import App from "./App";
+
+function Login() {
+  // React States
+  const [errorUname, setErrorUname] = useState("");
+  const [errorPass, setErrorPass] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [name, setname]=useState("")
+  const [pass, setpass]=useState("")
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    setErrorPass("");
+    setErrorUname("");
+    event.preventDefault();
+     if(name===database[0].username)
+     {
+        if(pass===database[0].password)
+        {
+          setIsSubmitted(true);
+        }
+        else
+        {
+             setErrorPass("invalid password")
+        }
+     }
+     else if(name===database[1].username)
+     {
+        if(pass===database[1].password)
+        {
+          setIsSubmitted(true);
+        }
+        else
+        {
+             setErrorPass("invalid password")
+        }
+     }
+     else
+     {
+            setErrorUname("invalid username")
+     }
+  };
+
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" value={name} 
+          onChange={(e) => {
+            setname(e.target.value);}} required />
+            {errorUname}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" value={pass}
+          onChange={(e) => {
+          setpass(e.target.value);}}
+          required />
+          {errorPass}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+          
+  if (isSubmitted) {
+    return <App></App>
+  } else {
+    return (
+        <div className="app">
+          <div className="login-form">
+            <div className="title">Sign In</div>
+            {renderForm}
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Login;
